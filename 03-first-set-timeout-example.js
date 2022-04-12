29e73cda56b62f7ec997f15b2cf07a36f63c060c
Warren <hello@warren.codes>
2022-04-12 11:27:17 -0600

    Add setTimeout examples.

---
+++ 03-first-set-timeout-example.js
@@ -0,0 +1,8 @@
+console.log('Beginning of script.');
+
+// Arguments: callbackFunction(), delay (in milliseconds)
+setTimeout(() => {
+    console.log('3000ms timeout has run!');
+}, 3000);
+
+console.log('End of script.');
